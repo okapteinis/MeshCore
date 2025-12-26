b72fc28cfdc6475cef1cd450f4e230dee6915aed
Ojārs Kapteinis <31920576+okapteinis@users.noreply.github.com>
2025-12-26 20:43:35 +0200

    Add conditional compilation guards for radio interface functions in target.h

--- variants/sensecap_indicator_d1l/target.h
+++ variants/sensecap_indicator_d1l/target.h
@@ -45,9 +45,12 @@ extern MomentaryButton user_btn;
 bool initStorage();
 bool testMapAccess();
 
+#if RADIO_DRIVER_AVAILABLE
 // Radio interface functions
 bool radio_init();
 uint32_t radio_get_rng_seed();
 void radio_set_params(float freq, float bw, uint8_t sf, uint8_t cr);
 void radio_set_tx_power(uint8_t dbm);
 mesh::LocalIdentity radio_new_identity();
+#endif
+
